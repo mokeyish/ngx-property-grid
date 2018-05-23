330c7e344d9751b1eb8e79f2e88c820299dff82d
mokeyish <mokeyish@hotmail.com>
2018-05-23 22:24:42 +0800

    1

--- dist/components/property-grid/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { PropertyGridItemMeta, $meta } from './property-grid-item-meta';
-export declare class PropertyGridModule {
-}
--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
 
-export * from './dist/components/property-grid';
+export * from './components/property-grid';
